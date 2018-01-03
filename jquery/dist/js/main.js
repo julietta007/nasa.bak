7e9866a0e4f45899114724a5a922b1b0f1340e04
juliettaurdiales@yahoo.comlsgit statusgit commit README.mdcd /var/www/mtbclsgit config --global user.name juliettaurdiales@yahoo.com <juliettaurdiales@yahoo.com>
2018-01-03 15:18:26 -0600

    practicing

--- jquery/dist/js/main.js
+++ jquery/dist/js/main.js
@@ -1,5 +1,4 @@
 //console.log(nasaKey);
-
 var apod = {
 
   //create a random apodDate
@@ -13,17 +12,14 @@ var apod = {
     let m = date.getMonth() + 1;
     let y = date.getFullYear();
 
-    if(m < 10){
-      m = '0' + m;
-}
+    if(m < 10){ m = '0' + m;
+    }
 
-    if(d < 10){
-      d = '0' + d;
+    if(d < 10){ d = '0' + d;
     }
 
     return `${y}-${m}-${d}`;
-
-  },
+    },
 
 buildDOM: function(result){
 
